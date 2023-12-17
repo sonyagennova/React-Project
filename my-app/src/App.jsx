5e4d7def2daf1ac531608196b8d71c04ea04abf2
Sonya Genova <sonyagennova@gmail.com>
2023-12-17 14:34:56 +0200

    Added authentication for logged in and logged out users

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,24 +1,39 @@
-import page from "page"
-import { About } from "./components/homePageSections/AboutSection"
-import { AllCategories } from "./components/homePageSections/AllCategories"
-import { BookCategory } from "./components/homePageSections/BookCategory"
 import { CategoriesPage } from "./components/otherPages/CategoriesPage"
-import { FamousBooks } from "./components/homePageSections/FamousBooks"
-import { Footer } from "./components/homePageSections/Footer"
-import { Header } from "./components/homePageSections/Header"
-import { topBooks } from "./props/topBooks"
-import { Info } from "./components/homePageSections/InfoSection"
 import { CategoryPage } from "./components/otherPages/CategoryPage"
 import { Login } from "./components/otherPages/LoginPage"
 import { RegisterPage } from "./components/otherPages/RegisterPage"
 import { CreatePage } from "./components/otherPages/createPage"
-import { ReadMore } from "./components/otherPages/ReadMoreINfo"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate, useRoutes } from "react-router-dom"
 import { HomePage } from "./components/homePageSections/homePage"
-import { useContext, useState } from "react"
+import { useEffect } from "react"
 
 
 function App() {
+
+  const PrivateRoute = ({ element }) => {
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      if (!localStorage.getItem('auth')) {
+        navigate('/');
+      }
+    }, [navigate]);
+  
+    return element();
+  };
+
+  const PublicRoute = ({ element }) => {
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      if (localStorage.getItem('auth')) {
+        navigate('/');
+      }
+    }, [navigate]);
+  
+    return element();
+  };
+
     return (
       <>
       <Routes>
@@ -26,9 +41,9 @@ function App() {
         <Route path="/categories" element={<CategoriesPage/>}/>
         <Route path="/category/:category" element={<CategoryPage/>}/>
         <Route path="/category/:category/:bookId" element={<CategoryPage />}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<RegisterPage/>}/>
-        <Route path="/add" element={<CreatePage/>}/>
+        <Route path="/login" element={<PublicRoute element={() => <Login/>}/>}/>
+        <Route path="/register" element={<PublicRoute element={() => <RegisterPage/>}/>}/>
+        <Route path="/add" element={<PrivateRoute element={() => <CreatePage />}/>}/>
       </Routes>
   </>
   
