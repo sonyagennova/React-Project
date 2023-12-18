cc04839a28666a903ccb936c7cbb88a2ac2bf83e
Sonya Genova <sonyagennova@gmail.com>
2023-12-18 15:35:48 +0200

    Updated Latest books service

--- my-app/src/utils/topBooksService.js
+++ my-app/src/utils/topBooksService.js
@@ -4,9 +4,10 @@ export const top = async () => {
     const response = await fetch(baseUrl);
     const result = await response.json();
 
-    const sortedBooks = result.sort((a, b) => b.publication_year - a.publication_year);
+    const sortedBooks = result
+    // .sort((a, b) => b.publication_year - a.publication_year);
 
-    const lastThreeBooks = sortedBooks.slice(0, 3);
+    const lastThreeBooks = sortedBooks.slice(-3);
 
     return lastThreeBooks;
 }
